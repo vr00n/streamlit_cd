38a367d016ccb206313b97be7236dbb458620ab3
Varun Adibhatla <vr00n@users.noreply.github.com>
2024-08-20 19:45:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ else:
                             'Category': category,
                             'Measure': measure_name,
                             'Measure Value': measure_value,
-                            'Rank': int(round(rank))
+                            'Rank': rank
                         })
 
                     measures_df = pd.DataFrame(measures_data)
